697283da3741fe06b2d5add355623e7695ae9829
bodizypha <jed.davis.email@gmail.com>
2016-11-23 13:24:21 -0600

    over haul 01

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,20 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
-gulp.task('bundle', function() {
+
+gulp.task('bundle',function(){
     return browserify({
-        entries: ['public/src/js/app.js'],
+        entries: ['public/js/app.js'],
         debug: true
     }).bundle()
     .on('error', function(error) {
-        console.log(error.toString());
-        this.emit('end');
+    console.log(error.toString());
+    this.emit('end');
     })
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest('public/js/'));
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('public/js/'));
 });
-
-gulp.task('sass', function() {
-    return gul.src('./public/src/scss/app.scss')
-    .pipe(sass().on('error',sass))
-})
 
 gulp.task('watch', function() {
     gulp.watch('public/src/js/**/*.js', ['bundle']);
